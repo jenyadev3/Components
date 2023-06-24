0fabd36bc3fc89c7d9ff82b45577a7d24616484c
Evgeniya <112981213+jenyadev3@users.noreply.github.com>
2023-06-24 20:04:17 +0300

    Add files via upload

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Item from "../Item.js";
+
+
+function ShopItemFunc(props) {
+  const {item} = props;
+  return (
+   <div className="main-content">
+   <h2>{item.brand}</h2>
+   <h1>{item.title}</h1>
+   <h3>{item.description}</h3>
+   <div className="description">
+    {item.descriptionFull}
+   </div>
+   <div className="highlight-window mobile">
+    <div className="highlight-overlay"></div>
+    </div>
+   <div className="divider"></div>
+   <div className="purchase-info">
+    <div className="price">{item.currency}{item.price}.00</div>
+    <button>Добавить в корзину</button>
+   </div>
+ </div>
+  )
+} 
+
+
+/* Классовый компонент
+class ShopItemFunc extends React.Component {
+  render() {
+    const {item} = this.props;
+    return (
+      <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">
+       {item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile">
+       <div className="highlight-overlay"></div>
+       </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+       <div className="price">{item.currency}{item.price}.00</div>
+       <button>Добавить в корзину</button>
+      </div>
+    </div>
+     ) 
+  }
+}
+*/
+
+ShopItemFunc.propTypes = {
+  brand: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  descriptionFull: PropTypes.string,
+  price: PropTypes.number,
+  currency: PropTypes.string
+}
+
+export default ShopItemFunc;
